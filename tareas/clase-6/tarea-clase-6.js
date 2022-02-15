98b88b1640918dd7bdd12f1f66e1e9176195062d
Enzo Juan Andrés Almada <99050038+Lenz-or@users.noreply.github.com>
2022-02-15 15:24:18 -0300

    Agrego funcionalidades de calcular el salario
    Se agregan funciones que calculan el promedio, el mayor y el menor salario.

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -71,6 +71,7 @@ $calcular.onclick = function(){
     document.querySelector("#edad-menor").textContent = calcularMenorEdad(document.querySelectorAll(".edad-integrante"));
     document.querySelector("#edad-promedio").textContent = calcularPromedio(document.querySelectorAll(".edad-integrante"));
     mostrarInformacion("#espacios-calculo");
+    mostrarInformacion("#boton-salario");
 }
 
 
@@ -120,22 +121,6 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor sala
 
 Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
 */
-function agregarEspacio(){
-    const $div = document.createElement('div');
-    $div.className = 'espacio-calculo';
-  
-    const $label = document.createElement('label');
-    $label.textContent = "Salario anual" ;
-  
-    const $input = document.createElement('input');
-    $input.type = 'number';
-  
-    $div.appendChild($label);
-    $div.appendChild($input);
-  
-   const $espacios = document.querySelector("#espacios-calculo");
-   $espacios.appendChild($div);
-  }
 
 const $agregar_salario = document.querySelector("#agregar-salario");
 $agregar_salario.onclick= function(){
@@ -147,10 +132,83 @@ $quitar_salario.onclick = function(){
     quitarEspacio()
 }
 
+const $calcular_salario = document.querySelector("#calcular-salario");
+$calcular_salario.onclick = function(){
+    
+    mostrarInformacion("#calculos");
+    document.querySelector('#salario-mayor').textContent = calcularMayorSalario(document.querySelectorAll('.espacio-calculo'));
+    document.querySelector('#salario-menor').textContent = calcularMenorSalario(document.querySelectorAll('.espacio-calculo'));
+    document.querySelector('#salario-promedio').textContent = calcularPromedioSalario(document.querySelectorAll('.espacio-calculo'));
+}
+
+function agregarEspacio(){
+    const $div = document.createElement('div');
+    $div.className = 'espacio-cal';
+  
+    const $label = document.createElement('label');
+    $label.textContent = "Salario anual" ;
+  
+    const $input = document.createElement('input');
+    $input.type = 'number';
+    $input.className= 'espacio-calculo'
+  
+    $div.appendChild($label);
+    $div.appendChild($input);
+  
+   const $espacios = document.querySelector("#espacios-calculo");
+   $espacios.appendChild($div);
+  }
+
+
 function quitarEspacio(){
     const $div = document.querySelector(".espacio-calculo");
     
     const $espacios = document.querySelector("#espacios-calculo");
     $espacios.removeChild($div);
+}
+
+
+
+
+function calcularMayorSalario(elemento){
+    const salarios = elemento;
+    let mayorSalario=0;
+    for(let i=0;i < salarios.length; i++){
+        if(i === 0){
+            mayorSalario = Number(salarios[i].value);
+        }
+        else if(salarios[i].value > mayorSalario){
+            mayorSalario = Number(salarios[i].value);
+        }
+    }
+    return mayorSalario;
+}
+
+
+function calcularMenorSalario(elemento){
+    const salarios = elemento;
+    let menorSalario=0;
+    for(let i=0;i < salarios.length; i++){
+        if(i === 0){
+            menorSalario = Number(salarios[i].value);
+        }
+        else if(salarios[i].value < menorSalario){
+            menorSalario = Number(salarios[i].value);
+        }
+    }
+    return menorSalario;
+}
+
+
+function calcularPromedioSalario(elemento){
+    const salarios = elemento;
+    let cantidad= salarios.length;
+    let sumaTotal = 0;
+    for(let i=0;i < salarios.length; i++){
+        sumaTotal += Number(salarios[i].value);
+    
+    }
+    
+    return sumaTotal / cantidad;
 }
 
